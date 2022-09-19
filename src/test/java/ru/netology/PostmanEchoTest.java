f95c3d358ee7a5837359706715e8135710692f10
avdeevaleksandr817 <avdeevaleksandr817@gmail.com>
2022-09-20 01:15:04 +0400

    test fail

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -21,7 +21,7 @@ public class PostmanEchoTest {
         // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("Иванов"))
+                .body("data",equalTo("ванов"))
         ;
 
     }
